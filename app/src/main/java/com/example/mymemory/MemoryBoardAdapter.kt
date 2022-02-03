4f06a972e8268b430956d2f7d1c3329ef4638a7d
JorritWillaert <jorritwillaert98@gmail.com>
2022-02-03 16:10:36 +0100

    Add margins

--- app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -11,6 +11,10 @@ import kotlin.math.min
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+    }
+
    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
        fun bind(position: Int) {
             //No op
@@ -18,13 +22,14 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var cardWidth = parent.width / 2
-        var cardHeight = parent.height / 4
+        var cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        var cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
         val cardSideLength = min(cardWidth, cardHeight)
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
-        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
         layoutParams.width = cardSideLength
         layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
         return ViewHolder(view)
     }
 
