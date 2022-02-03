866e0c72f962392ff1ec4972076e26bbded00466
JorritWillaert <jorritwillaert98@gmail.com>
2022-02-03 20:24:39 +0100

    Add colors to visualize progress

--- app/src/main/java/com/example/mymemory/MainActivity.kt
+++ app/src/main/java/com/example/mymemory/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mymemory
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymemory.models.BoardSize
@@ -36,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
 
         memoryGame = MemoryGame(boardSize)
         adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
@@ -64,6 +67,13 @@ class MainActivity : AppCompatActivity() {
         // Actually flip over the card
         if (memoryGame.flipCard(position)) {
             Log.i(TAG, "Found a match! Num pairs found: ${memoryGame.numPairsFound}")
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+
+            ) as Int
+            tvNumPairs.setTextColor(color)
             tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
             if (memoryGame.haveWonGame()) {
                 Snackbar.make(clRoot, "You won! Congratulations.", Snackbar.LENGTH_LONG).show()
