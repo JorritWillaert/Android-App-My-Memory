bc5e24144aae63ebebad09e032477f3d26eb05f5
JorritWillaert <jorritwillaert98@gmail.com>
2022-02-03 20:16:56 +0100

    Update number of moves and number of found pairs

--- app/src/main/java/com/example/mymemory/models/MemoryGame.kt
+++ app/src/main/java/com/example/mymemory/models/MemoryGame.kt
@@ -7,6 +7,7 @@ class MemoryGame(private val boardSize: BoardSize) {
     val cards: List<MemoryCard>
     var numPairsFound = 0
     private var indexOfSingleSelectedInt: Int? = null
+    private var numCardFlips = 0
 
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
@@ -15,6 +16,7 @@ class MemoryGame(private val boardSize: BoardSize) {
     }
 
     fun flipCard(position: Int) : Boolean {
+        numCardFlips++
         val card = cards[position]
         var foundMatch = false
         // Three cases
@@ -59,5 +61,9 @@ class MemoryGame(private val boardSize: BoardSize) {
 
     fun isCardFaceUp(position: Int): Boolean {
         return cards[position].isFaceUp
+    }
+
+    fun getNumMoves(): Int {
+        return numCardFlips / 2
     }
 }
