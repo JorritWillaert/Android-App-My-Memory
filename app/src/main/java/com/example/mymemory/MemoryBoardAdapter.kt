681c69cdb7dac1d8cab92d53b918927c3d815699
JorritWillaert <jorritwillaert98@gmail.com>
2022-02-06 20:17:15 +0100

    Add a placeholder image, in case you start playing while still downloading the images

--- app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -39,7 +39,7 @@ class MemoryBoardAdapter(
            val memoryCard = cards[position]
            if (memoryCard.isFaceUp) {
                if (memoryCard.imageUrl != null) { // Valid custom image
-                   Picasso.get().load(memoryCard.imageUrl).into(imageButton)
+                   Picasso.get().load(memoryCard.imageUrl).placeholder(R.drawable.ic_image).into(imageButton)
                } else {
                    imageButton.setImageResource(memoryCard.identifier)
                }
