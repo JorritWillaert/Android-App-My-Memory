f1b145f1b4e95427170ff0b960e9c69364444871
JorritWillaert <jorritwillaert98@gmail.com>
2022-02-06 10:23:09 +0100

    Add logic for when to enable the save button

--- app/src/main/java/com/example/mymemory/CreateActivity.kt
+++ app/src/main/java/com/example/mymemory/CreateActivity.kt
@@ -6,10 +6,14 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -25,6 +29,8 @@ class CreateActivity : AppCompatActivity() {
         private const val PICk_PHOTO_CODE = 655 // Random number
         private const val READ_EXTERNAL_PHOTOS_CODE = 248 // Random number
         private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        private const val MIN_GAME_NAME_LENGTH = 3
+        private const val MAX_GAME_NAME_LENGTH = 14
     }
 
     private lateinit var rvImagePicker: RecyclerView
@@ -48,6 +54,17 @@ class CreateActivity : AppCompatActivity() {
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+
+        etGameName.filters = arrayOf(InputFilter.LengthFilter(MAX_GAME_NAME_LENGTH))
+        etGameName.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                btnSave.isEnabled = shouldEnableSaveButton()
+            }
+        })
 
         adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListerer {
             override fun onPlaceholderClicked() {
@@ -110,6 +127,18 @@ class CreateActivity : AppCompatActivity() {
         }
         adapter.notifyDataSetChanged()
         supportActionBar?.title = "Choose pictures (${chosenImageUris.size} / $numImagesRequired)"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+        // Check if we should enable the save button or not
+        if (chosenImageUris.size != numImagesRequired) {
+            return false
+        }
+        if (etGameName.text.isBlank() || etGameName.text.length < MIN_GAME_NAME_LENGTH) {
+            return false
+        }
+        return true
     }
 
     private fun launchIntentForPhotos() {
